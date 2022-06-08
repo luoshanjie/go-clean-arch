b3614610f649d8720bae2d97197e5574da7802b6
luoshanjie <luoshanjie@gmail.com>
2022-06-08 22:59:56 +0800

    base server

--- lesson_one/main.go
+++ lesson_one/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handleAddUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "status response")
+}
 
 func main() {
-	fmt.Println("clean arc")
+	http.HandleFunc("/api/v1/user/add", handleAddUser)
+	log.Fatal(http.ListenAndServe(":10086", nil))
 }
